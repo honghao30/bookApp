00b078abb046dcc34c676072820c1215bc76d258
hongsh <hongsh@the-51.com>
2024-01-26 16:17:16 +0900

    오디오복구

--- src/books/AudioList.tsx
+++ src/books/AudioList.tsx
@@ -1,26 +1,47 @@
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, Key } from "react";
-import styled from "styled-components";
+import * as React from 'react';
+import Accordion, { AccordionSlots } from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Fade from '@mui/material/Fade';
 
-const InsetList: React.FC = ({ dataList }) => {
+export default function AccordionUsage({ audioList }) {
+  const [expanded, setExpanded] = React.useState(false);
 
-const List = styled.ul `
-  display: block;
-`;
-const ListItem = styled.li `
-  font-size: 15px;
-  padding: 6px 0;
-`
-
+  const handleExpansion = () => {
+    setExpanded((prevExpanded) => !prevExpanded);
+  };  
   return (
-    <List>
-      {dataList.map((book: {
+    <div>
+      <Accordion
+        expanded={expanded}
+        onChange={handleExpansion}
+        slots={{ transition: Fade as AccordionSlots['transition'] }}
+        slotProps={{ transition: { timeout: 400 } }}
+        sx={{
+          '& .MuiAccordion-region': { height: expanded ? 'auto' : 0 },
+          '& .MuiAccordionDetails-root': { display: expanded ? 'block' : 'none' },
+        }}
+      >     
+      {audioList.map((audio: {
         [x: string]: ReactNode; title: string | number | boolean | ReactElement<unknown, string | JSXElementConstructor<unknown>> | Iterable<ReactNode> | ReactPortal | null | undefined; 
 }, index: Key | null | undefined) => (
-        <ListItem key={index}>
-          {book.cates} 
-        </ListItem>
+        <>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          {audio.subject} 
+        </AccordionSummary>
+         <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+         </AccordionDetails>
+         </>
       ))}            
-    </List>
+      </Accordion>
+    </div>
   );
 }
-export default InsetList
--- src/books/BottomNav.tsx
+++ src/books/BottomNav.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 import HomeIcon from '@mui/icons-material/Home';
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
 import HeadsetIcon from '@mui/icons-material/Headset';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import SettingsIcon from '@mui/icons-material/Settings';
+import navigation from '@/baejh/components/Navigation.vue';
+import { useNavigate } from 'react-router-dom';
 
 const BottomNav: React.FC = () => {
   const [value, setValue] = React.useState('recents');
-
+  const navigate = useNavigate();
+  
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
+
+  const alertMsg = () => {
+    alert('열심히 준비중입니다.');
+  }
+  const alertMsg2 = () => {
+    alert('열심히 준비중입니다.');
+  }
+  const goHome = () => {
+    navigate('/bookMain')
+  }
 
   return (
     <BottomNavigation sx={{ width: '100%', position: 'fixed', bottom: 0, left: 0, right: 0  }} 
@@ -22,21 +34,24 @@ const BottomNav: React.FC = () => {
         label="홈"
         value="홈"
         icon={<HomeIcon />}
+        onClick={goHome}
       />
       <BottomNavigationAction
         label="검색"
         value="검색"
         icon={<TravelExploreIcon />}
+        onClick={ alertMsg }
       />
-      <BottomNavigationAction
+      {/* <BottomNavigationAction
         label="오디오"
         value="오디오"
         icon={<HeadsetIcon />}
-      />
+      /> */}
      <BottomNavigationAction
         label="절기"
         value="절기"
         icon={<CalendarMonthIcon />}
+        onClick={alertMsg2}
       />      
      <BottomNavigationAction
         label="설정"
