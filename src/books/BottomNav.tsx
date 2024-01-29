928e77b19082d703758f07b5eb46527fbee9fcc9
hongsh <hongsh@the-51.com>
2024-01-29 11:16:04 +0900

    책

--- src/books/BottomNav.tsx
+++ src/books/BottomNav.tsx
@@ -1,20 +1,49 @@
-import * as React from 'react';
+import React, { useState, useCallback, useEffect } from "react";
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import HomeIcon from '@mui/icons-material/Home';
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
-import HeadsetIcon from '@mui/icons-material/Headset';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import SettingsIcon from '@mui/icons-material/Settings';
-import navigation from '@/baejh/components/Navigation.vue';
 import { useNavigate } from 'react-router-dom';
+import { Drawer } from "@mui/material";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import Switch from '@mui/material/Switch';
+import WifiIcon from '@mui/icons-material/Wifi';
+import Slider from '@mui/material/Slider';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 
 const BottomNav: React.FC = () => {
   const [value, setValue] = React.useState('recents');
   const navigate = useNavigate();
+  const [drawer, setDrawer] = useState(false);
+  const openDrawer = useCallback(() => setDrawer(true), []);
+  const closeDrawer = useCallback(() => setDrawer(false), []);
   
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
+  };
+
+  const [checked, setChecked] = React.useState(['wifi']);
+
+  const handleToggle = (value: string) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
   };
 
   const alertMsg = () => {
@@ -28,37 +57,83 @@ const BottomNav: React.FC = () => {
   }
 
   return (
-    <BottomNavigation sx={{ width: '100%', position: 'fixed', bottom: 0, left: 0, right: 0  }} 
-      value={value} onChange={handleChange}>
+    <>
+      <BottomNavigation sx={{ width: '100%', position: 'fixed', bottom: 0, left: 0, right: 0  }} 
+        value={value} onChange={handleChange}>
+        <BottomNavigationAction
+          label="홈"
+          value="홈"
+          icon={<HomeIcon />}
+          onClick={goHome}
+        />
+        <BottomNavigationAction
+          label="검색"
+          value="검색"
+          icon={<TravelExploreIcon />}
+          onClick={ alertMsg }
+        />
+        {/* <BottomNavigationAction
+          label="오디오"
+          value="오디오"
+          icon={<HeadsetIcon />}
+        /> */}
       <BottomNavigationAction
-        label="홈"
-        value="홈"
-        icon={<HomeIcon />}
-        onClick={goHome}
-      />
+          label="절기"
+          value="절기"
+          icon={<CalendarMonthIcon />}
+          onClick={alertMsg2}
+        />      
       <BottomNavigationAction
-        label="검색"
-        value="검색"
-        icon={<TravelExploreIcon />}
-        onClick={ alertMsg }
-      />
-      {/* <BottomNavigationAction
-        label="오디오"
-        value="오디오"
-        icon={<HeadsetIcon />}
-      /> */}
-     <BottomNavigationAction
-        label="절기"
-        value="절기"
-        icon={<CalendarMonthIcon />}
-        onClick={alertMsg2}
-      />      
-     <BottomNavigationAction
-        label="설정"
-        value="설정"
-        icon={<SettingsIcon />}
-      />                  
-    </BottomNavigation>    
+          label="설정"
+          value="설정"
+          icon={<SettingsIcon />}
+          onClick={openDrawer}
+        />    
+            
+      </BottomNavigation>  
+      <Drawer
+          anchor={"bottom"}
+          open={drawer}
+          onClose={closeDrawer}
+          PaperProps={{
+            style: {
+              overflow: "hidden"
+            }
+          }}
+        >
+          <div className="setting-list">
+            <List
+              sx={{ width: '100%', maxWidth: '100%', bgcolor: 'background.paper' }}
+              subheader={<ListSubheader>설정</ListSubheader>}
+            >
+              <ListItem>
+                <ListItemIcon>
+                  <AccessTimeFilledIcon />
+                </ListItemIcon>
+                <ListItemText id="switch-list-label-wifi" primary="다크모드" />
+                <Switch
+                  edge="end"
+                  onChange={handleToggle('darkMod')}
+                  checked={checked.indexOf('darkMod') !== -1}
+                  inputProps={{
+                    'aria-labelledby': 'switch-list-label-wifi',
+                  }}
+                />
+              </ListItem>
+              <ListItem>
+               <Box sx={{ width: '100%' }}>
+                  <Typography id="input-slider" gutterBottom>
+                    폰트 사이즈
+                  </Typography>  
+                  <Grid item xs>
+                    <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+                  </Grid>  
+                </Box>           
+              </ListItem>
+            </List>            
+          </div>          
+        </Drawer>  
+    </>  
     )
 }
 
--- src/books/ReadTapeDetail.tsx
+++ src/books/ReadTapeDetail.tsx
@@ -3,8 +3,13 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useLocation } from 'react-router-dom';
 import styled from "styled-components";
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import { useTheme } from '@mui/material/styles';
+import { SxProps } from '@mui/material/styles';
+import Zoom from '@mui/material/Zoom';
+import { green } from '@mui/material/colors';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 const SubTitle = styled.p `
   font-size: 20px;
@@ -25,12 +30,37 @@ const ButtonArea = styled.div `
   justify-content: center;
   align-items: center;
 `
+const fabStyle = {
+  position: 'fixed',
+  bottom: 56,
+  right: 16,
+};
+
+const fabGreenStyle = {
+  color: 'common.white',
+  bgcolor: green[500],
+  '&:hover': {
+    bgcolor: green[600],
+  },
+};
 
 const ReadTapeDetail: React.FC = () => {  
   const [book, setBook] = useState(null);
   const { typeId } = useParams();
   const location = useLocation();
   const { cates, index } = location.state;
+  const [value, setValue] = useState(0);
+  const theme = useTheme();
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  const fabs = [
+    {
+      color: 'primary' as 'primary',
+      sx: fabStyle as SxProps,
+      icon: <KeyboardArrowUpIcon />,
+      label: '위로',
+    }
+  ];
 
   useEffect(() => {
     const fetchBook = async () => {
@@ -39,17 +69,39 @@ const ReadTapeDetail: React.FC = () => {
       setBook(response.data);
     };
     fetchBook();
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 400) {
+            setShowTopBtn(true);
+        } else {
+            setShowTopBtn(false);
+        }
+    });    
   }, [typeId]);
 
+  const goToTop = () => {
+      window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+      });
+  };
   if (!book) {
     return <div>Loading...</div>;
   }
-
   return (
     <div className='book-content'>
       <SubTitle>
         {cates}
       </SubTitle>
+      {showTopBtn && fabs.map((fab, index) => (
+        <Zoom
+          key={fab.color}
+          in={value === index}
+        >
+          <Fab sx={fab.sx} aria-label={fab.label} color={fab.color} size="medium" onClick={goToTop}>
+            {fab.icon}
+          </Fab>
+        </Zoom>
+      ))}
       <BookContent dangerouslySetInnerHTML={{ __html: book[index].content }} />  
     </div>
   )
