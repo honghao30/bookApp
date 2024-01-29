b9429235aba2756e619a2ae35fc9ab4168177c3e
hongsh <hongsh@the-51.com>
2024-01-29 14:21:29 +0900

    소스정리~

--- src/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <footer className="footer__wrap">
-        <p>Copyright ⓒ 2024. All rights reserved. </p>
-      </footer>
-    )
-  }
-  
-  export default Footer
